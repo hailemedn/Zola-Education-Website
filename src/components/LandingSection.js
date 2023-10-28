0921f942fee37878276f4c585ea60c9e9a28d643
hailemedn <hailemedhin.k@gmail.com>
2023-10-28 19:46:18 +0300

    made the navigation links bigger and fixed the image  hiding the menu in mobile form

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,4 +1,4 @@
-import { Avatar, Heading, VStack, HStack, Button, Text } from "@chakra-ui/react";
+import { Avatar, Heading, VStack, HStack, Button, Text, Image } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faYoutube, faTiktok } from "@fortawesome/free-brands-svg-icons";
@@ -23,17 +23,17 @@ const LandingSection = () => {
             justifyContent="center"
 
             >
-            <HStack gap="20" alignItems="start">  
+            <HStack gap="4" justifyContent="center" flexWrap="wrap" p={10} m={3}>  
                 <VStack>
-                    <Avatar src={ZeLogo} size="full"></Avatar>
+                    <Image src={ZeLogo} alt="Ze Logo" borderRadius="full"></Image>
                     
                 </VStack> 
-                <VStack p="30px" alignItems="flex-start">
+                <VStack p="30px" alignItems="center">
                     <Heading>Zola Education</Heading>
                     <Heading as="h2" size="md">Educational Content in Amharic</Heading>
-                    <VStack alignItems="flex-start" mt="15px">
+                    <VStack mt="15px">
                         <Text color="red.400" fontSize="2xl" fontWeight="semibold">Visit our Socials</Text>
-                        <HStack spacing={6} justifyContent= "left"> 
+                        <HStack spacing={6}> 
                             {socials.map(({icon, url}) => (
                                 <a href={url} key={url}><FontAwesomeIcon icon={icon} size='2x'></FontAwesomeIcon></a>
                             ))}
