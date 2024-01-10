6eaa85a5ddcecfc58d2b8d641dd4b0079754c064
hailemedn <hailemedhin.k@gmail.com>
2024-01-10 21:01:42 +0300

    add a Youtube channel showcase section

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,6 @@ import "./styles.css";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useState, useRef, useEffect } from "react";
 
-
-
 const Header = () => {
   // state = {clicked: false}
   // handleClick = () => {
@@ -56,58 +54,60 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="container wrapper" ref={headerRef}>
-      <h1>
-        <a href="#home" onClick={handleNavigation("home")}>
-          ZEs
-        </a>
-      </h1>
-      <nav>
-        <ul id="navbar" className={clicked ? "#navbar active" : "#navbar"}>
-        <li>
-            <a
-              className="red"
-              href="#contactus"
-              onClick={handleNavigation("contactus")}
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              className="red"
-              href="#contactus"
-              onClick={handleNavigation("contactus")}
-            >
-              YouTube
-            </a>
-          </li>
-          <li>
-            <a
-              className="red"
-              href="#aboutus"
-              onClick={handleNavigation("aboutus")}
-            >
-              Courses
-            </a>
-          </li>
-          <li>
-            <a
-              className="red"
-              href="#contactus"
-              onClick={handleNavigation("contactus")}
-            >
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <div id="mobile" onClick={handleClick}>
-        <FontAwesomeIcon
-          icon={clicked ? faTimes : faBars}
-          size="2xl"
-          cursor="pointer"
-        ></FontAwesomeIcon>
+    <header className="container" ref={headerRef}>
+      <div className="wrapper">
+        <h1>
+          <a href="#home" onClick={handleNavigation("home")}>
+            ZEs
+          </a>
+        </h1>
+        <nav>
+          <ul id="navbar" className={clicked ? "#navbar active" : "#navbar"}>
+            <li>
+              <a
+                className="red"
+                href="#contactus"
+                onClick={handleNavigation("contactus")}
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                className="red"
+                href="#contactus"
+                onClick={handleNavigation("contactus")}
+              >
+                YouTube
+              </a>
+            </li>
+            <li>
+              <a
+                className="red"
+                href="#aboutus"
+                onClick={handleNavigation("aboutus")}
+              >
+                Courses
+              </a>
+            </li>
+            <li>
+              <a
+                className="red"
+                href="#contactus"
+                onClick={handleNavigation("contactus")}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div id="mobile" onClick={handleClick}>
+          <FontAwesomeIcon
+            icon={clicked ? faTimes : faBars}
+            size="2xl"
+            cursor="pointer"
+          ></FontAwesomeIcon>
+        </div>
       </div>
     </header>
   );
--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,8 +1,13 @@
-
+import youTubeImg from '../images/Youtube.png'
 const Youtube = () => {
     return(
-        <section id="youtube-section">
-            <img src="" alt="" />
+        <section id="youtube-section" className='wrapper'>
+            <img src={youTubeImg} alt="Zolaxet Youtube" />
+            <div className="yt-description">
+                <h1>Zolax on Youtube</h1>
+                <p>Our YouTube Channel has over 10,000 Subscribers with 100+ free Courses </p>
+                <button className="btn primary">Go To Channel</button>
+            </div>
         </section>
     )
 }
